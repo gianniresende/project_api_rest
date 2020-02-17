e9744494718a4b7c9dea1622e825bb38ef741900
gianniresende <gianniresende@gmail.com>
2020-02-16 22:02:09 -0300

    crud

---
+++ src/app/models/projects.js
@@ -0,0 +1,28 @@
+const mongoose = require('../../database');
+
+const ProjetctSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    require: true,
+  },
+  tasks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task',
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Projetct = mongoose.model('Projetct', ProjetctSchema);
+module.exports = Projetct;
